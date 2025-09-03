5f6f48bdb70cee137a5bb7d3c3eb8d785c103e31
VPS - MES4game <mes4.game@gmail.com>
2025-09-03 13:21:49 +0200

    fix

--- back/src/controllers/activity.controller.ts
+++ back/src/controllers/activity.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { DB } from '@/config/db';
-import { ACTIVITY_SCHEMA } from '@/models/activity.models';
+import { ACTIVITY_SCHEMA, ENTRY_SCHEMA } from '@/models/activity.models';
 import { SCORE_SCHEMA } from '@/models/common.models';
 import { rawObjToSchema } from '@/utils/transfom';
 import { RowDataPacket } from 'mysql2';
@@ -45,7 +45,7 @@ export async function getActivityRecords(req: Request, res: Response) {
     try {
         const query ='SELECT * FROM `entry` WHERE `activity_id` = ?';
         const [result] = await DB.execute<RowDataPacket[]>(query, [req.params.id]);
-        const scores = result.map((value) => rawObjToSchema(value, SCORE_SCHEMA));
+        const scores = result.map((value) => rawObjToSchema(value, ENTRY_SCHEMA));
         res.status(200).json(scores);
     } catch (_) {
         res.status(500).json({ error: "Error fetching activity scores" });
@@ -57,8 +57,9 @@ export async function postActivityRecord(req: Request, res: Response) {
         const { password } = req.body as { password: string };
         if (password !== process.env.VPS_SITE_BACK_PASS) { res.status(500).send("Not authenticated"); return; }
         const { activity_id, team_id, team_num, room_id, slot_id, points, bonus, comment } = req.body as { activity_id: number, team_id: number, team_num: number, room_id: number, slot_id: number, points: number, bonus: number, comment: string };
+        console.log({ activity_id, team_id, team_num, room_id, slot_id, points, bonus, comment });
         const query ='INSERT INTO `entry` (`activity_id`, `team_id`, `team_num`, `room_id`, `slot_id`, `points`, `bonus`, `comment`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        await DB.execute(query, [activity_id, team_id, team_num, room_id, slot_id, points, bonus, comment]);
+        console.log(await DB.execute(query, [activity_id, team_id, team_num, room_id, slot_id, points, bonus, comment]));
         res.status(200).send("Done");
     } catch (_) {
         res.status(500).json({ error: "Error adding a record" });
@@ -71,7 +72,7 @@ export async function deleteActivityRecord(req: Request, res: Response) {
         if (password !== process.env.VPS_SITE_BACK_PASS) { res.status(500).send("Not authenticated"); return; }
         const { activity_id, team_id, team_num, room_id, slot_id } = req.body as { activity_id: number, team_id: number, team_num: number, room_id: number, slot_id: number };
         const query ='DELETE FROM `entry` WHERE `activity_id` = ? AND `team_id` = ? AND `team_num` = ? AND `room_id` = ? AND `slot_id` = ?';
-        await DB.execute(query, [activity_id, team_id, team_num, room_id, slot_id]);
+        console.log(await DB.execute(query, [activity_id, team_id, team_num, room_id, slot_id]));
         res.status(200).send("Done");
     } catch (_) {
         res.status(500).json({ error: "Error removing a record" });
